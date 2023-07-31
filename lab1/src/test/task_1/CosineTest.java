573e104f47bbac835bf9899777bd58f5f6e9f955
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2023-07-31 20:39:52 +0300

    Add files via upload

--- lab1/src/test/task_1/CosineTest.java
+++ lab1/src/test/task_1/CosineTest.java
@@ -1,74 +1,144 @@
 package test.task_1;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.api.DisplayName;
 import main.task_1.Cosine;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CosineTest {
+    Cosine cosine;
+    static Cosine cosinePrecise;
+    final static int DECIMAL_PLACES = 10;
 
-    @Test
-    @DisplayName("Init Cosine object with precision values")
-    void initCosine() {
-        /* Init with zero precision value*/
-        Cosine cosineZero = new Cosine(0);
-        assertEquals(0, cosineZero.getDecimal_places());
-        assertEquals(1.0D, cosineZero.getPrecision());
+    @BeforeAll
+    public static void initCosinePrecise() {
+        cosinePrecise = new Cosine(DECIMAL_PLACES);
+    }
 
+    @BeforeEach
+    public void initCosine() {
+        cosine = new Cosine();
+    }
 
-        /* Init with negative precision value*/
-        Cosine cosineNegOne = new Cosine(-1);
-        assertEquals(0, cosineNegOne.getDecimal_places());
-        assertEquals(1.0D, cosineNegOne.getPrecision());
+//    @ParameterizedTest
+//    @ValueSource( ints = {})
+//    @DisplayName("Init Cosine object with precision values")
+//    void initCosine(int precision) {
+//        /* Init with zero precision value*/
+//        Cosine cosineZero = new Cosine(0);
+//        assertEquals(0, cosineZero.getDecimal_places());
+//        assertEquals(1.0D, cosineZero.getPrecision());
+//
+//
+//        /* Init with negative precision value*/
+//        Cosine cosineNegOne = new Cosine(-1);
+//        assertEquals(0, cosineNegOne.getDecimal_places());
+//        assertEquals(1.0D, cosineNegOne.getPrecision());
+//
+//        Cosine cosineNegTen = new Cosine(-10);
+//        assertEquals(0, cosineNegTen.getDecimal_places());
+//        assertEquals(1.0D, cosineNegTen.getPrecision());
+//
+//
+//        /* Init with positive precision value */
+//        Cosine cosineOne = new Cosine(1);
+//        assertEquals(1, cosineOne.getDecimal_places());
+//        assertEquals(0.1D, cosineOne.getPrecision());
+//
+//        Cosine cosineTen = new Cosine(10);
+//        assertEquals(10, cosineTen.getDecimal_places());
+//        assertEquals(0.0000000001D, cosineTen.getPrecision());
+//    }
 
-        Cosine cosineNegTen = new Cosine(-10);
-        assertEquals(0, cosineNegTen.getDecimal_places());
-        assertEquals(1.0D, cosineNegTen.getPrecision());
+    @ParameterizedTest
+    @ValueSource( ints = {0})
+    @DisplayName("Test cosines absolute error if precision is ZERO decimal places.")
+    void testZeroPrecision (int precision) {
+        cosine.setDecimal_places(precision);
+        assertEquals(1.0D, cosine.getPrecision());
+    }
 
-
-        /* Init with positive precision value */
-        Cosine cosineOne = new Cosine(1);
-        assertEquals(1, cosineOne.getDecimal_places());
-        assertEquals(0.1D, cosineOne.getPrecision());
-
-        Cosine cosineTen = new Cosine(10);
-        assertEquals(10, cosineTen.getDecimal_places());
-        assertEquals(0.0000000001D, cosineTen.getPrecision());
+    @ParameterizedTest
+    @ValueSource( ints = {-1, -10})
+    @DisplayName("Test cosines absolute error if decimal places value is NEGATIVE.")
+    void testNegativePrecision (int precision) {
+        cosine.setDecimal_places(precision);
+        assertEquals(1.0D, cosine.getPrecision());
     }
 
     @Test
-    @DisplayName("Calculate cosine in extrema point with precision value = 10")
-    void calculateCosineExtremaPoint() {
-        final int DECIMAL_PLACES = 10;
-        Cosine cosine = new Cosine(DECIMAL_PLACES);
+    @DisplayName("Test cosines absolute error if decimal places value is POSITIVE.")
+    void testPositivePrecision () {
+        cosine.setDecimal_places(10);
+        assertEquals(0.0000000001D, cosine.getPrecision());
+    }
 
-        /* Check center*/
-        assertEquals(1, cosine.apply(0D));
 
-        /* Check relative maxima*/
-        assertEquals(1, cosine.apply(Math.toRadians(360)));
-        assertEquals(1, cosine.apply(Math.toRadians(3600)));
-        assertEquals(1, cosine.apply(Math.toRadians(-360)));
-        assertEquals(1, cosine.apply(Math.toRadians(-3600)));
+//    @Test
+//    @DisplayName("Calculate cosine in extrema point with precision value = 10")
+//    void calculateCosineExtremaPoint() {
+//        final int DECIMAL_PLACES = 10;
+//        Cosine cosine = new Cosine(DECIMAL_PLACES);
+//
+//         Check center
+//        assertEquals(1, cosine.apply(0D));
+//
+//         Check relative maxima
+//        assertEquals(1, cosine.apply(Math.toRadians(360)));
+//        assertEquals(1, cosine.apply(Math.toRadians(3600)));
+//        assertEquals(1, cosine.apply(Math.toRadians(-360)));
+//        assertEquals(1, cosine.apply(Math.toRadians(-3600)));
+//
+//         Check relative minima
+//        assertEquals(-1, cosine.apply(Math.toRadians(180)));
+//        assertEquals(-1, cosine.apply(Math.toRadians(180 + 3600)));
+//        assertEquals(-1, cosine.apply(Math.toRadians(-180)));
+//        assertEquals(-1, cosine.apply(Math.toRadians(-180 - 3600)));
+//
+//         Check concave upward
+//        assertEquals(0, cosine.apply(Math.toRadians(270)));
+//        assertEquals(0, cosine.apply(Math.toRadians(270 + 3600)));
+//        assertEquals(0, cosine.apply(Math.toRadians(-90)));
+//        assertEquals(0, cosine.apply(Math.toRadians(-90 - 3600)));
+//
+//         Check concave downward
+//        assertEquals(0, cosine.apply(Math.toRadians(90)));
+//        assertEquals(0, cosine.apply(Math.toRadians(90 + 3600)));
+//        assertEquals(0, cosine.apply(Math.toRadians(-270)));
+//        assertEquals(0, cosine.apply(Math.toRadians(-270 - 3600)));
+//    }
 
-        /* Check relative minima*/
-        assertEquals(-1, cosine.apply(Math.toRadians(180)));
-        assertEquals(-1, cosine.apply(Math.toRadians(180 + 3600)));
-        assertEquals(-1, cosine.apply(Math.toRadians(-180)));
-        assertEquals(-1, cosine.apply(Math.toRadians(-180 - 3600)));
+    @ParameterizedTest
+    @ValueSource( ints = {0, 360,3600, -360, -3600})
+    @DisplayName("Test cosine value in RELATIVE MAXIMA.")
+    void testRelativeMaxima (int angle) {
+        assertEquals(1, cosine.apply(Math.toRadians(angle)));
+    }
 
-        /* Check concave upward*/
-        assertEquals(0, cosine.apply(Math.toRadians(270)));
-        assertEquals(0, cosine.apply(Math.toRadians(270 + 3600)));
-        assertEquals(0, cosine.apply(Math.toRadians(-90)));
-        assertEquals(0, cosine.apply(Math.toRadians(-90 - 3600)));
+    @ParameterizedTest
+    @ValueSource( ints = {180, 180 + 3600, -180, -180 - 3600})
+    @DisplayName("Test cosine value in RELATIVE MINIMA.")
+    void testRelativeMinima (int angle) {
+        assertEquals(-1, cosine.apply(Math.toRadians(angle)));
+    }
 
-        /* Check concave downward*/
-        assertEquals(0, cosine.apply(Math.toRadians(90)));
-        assertEquals(0, cosine.apply(Math.toRadians(90 + 3600)));
-        assertEquals(0, cosine.apply(Math.toRadians(-270)));
-        assertEquals(0, cosine.apply(Math.toRadians(-270 - 3600)));
+    @ParameterizedTest
+    @ValueSource( ints = {270, 270 + 3600, -90, -90 - 3600})
+    @DisplayName("Test cosine value in CONCAVE UPWARD point.")
+    void testConcaveUpward (int angle) {
+        assertEquals(0, cosine.apply(Math.toRadians(angle)));
+    }
+
+    @ParameterizedTest
+    @ValueSource( ints = {90, 90 + 3600, -270, -270 - 3600})
+    @DisplayName("Test cosine value in CONCAVE DOWNWARD point.")
+    void testConcaveDownward (int angle) {
+        assertEquals(0, cosine.apply(Math.toRadians(angle)));
     }
 
     @Test
